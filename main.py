0666f949ed1e23e1ec48be7aac07d84c8ff71ec9
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2026-03-29 15:54:04 +0530

    Predicting signs of diabetes

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ data = pd.read_csv("Data.csv")
 data = data.drop(columns=["Date","Comment","Extra"])
 data = data.fillna(0)
 
-data["Status"] = (data.loc[:,data.columns.str.contains("Before",case=False)] > 99 ).any(axis=1)
-data["Status"] = data["Status"] | (data.loc[:,data.columns.str.contains("after",case=False)] > 140 ).any(axis=1)
+before_data = (data.loc[:,data.columns.str.contains("Before",case=False)] > 99 ).sum(axis=1)        # As True Means 1. Sums of Trues = Total number of Trues
+after_data = (data.loc[:,data.columns.str.contains("after",case=False)] > 140 ).sum(axis=1)
+
+row_datas = before_data + after_data
+
+data["Status"] = row_datas >=3 
 
 print(data)
