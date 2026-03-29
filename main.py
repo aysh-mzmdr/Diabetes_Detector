63d04348d018a9dab691bedf25e597d20607e2b3
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2026-03-29 16:14:44 +0530

    Categorizing patients as Diabetic, Prediabetic and Normal

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv("Data.csv")
 data = data.drop(columns=["Date","Comment","Extra"])
 data = data.fillna(0)
 
-before_data = (data.loc[:,data.columns.str.contains("Before",case=False)] > 99 ).sum(axis=1)        # As True Means 1. Sums of Trues = Total number of Trues
-after_data = (data.loc[:,data.columns.str.contains("after",case=False)] > 140 ).sum(axis=1)
+diabetic_before_data = (data.loc[:,data.columns.str.contains("Before",case=False)] > 126 ).sum(axis=1)        # As True Means 1. Sums of Trues = Total number of Trues
+diabetic_after_data = (data.loc[:,data.columns.str.contains("after",case=False)] > 200 ).sum(axis=1)
 
-row_datas = before_data + after_data
+diabetic_row_datas = diabetic_before_data + diabetic_after_data
 
-data["Status"] = row_datas >=3 
+diabetic = diabetic_row_datas >=3
+
+
+prediabetic_before_data = (data.loc[:,data.columns.str.contains("Before",case=False)] > 99 ).sum(axis=1)        # As True Means 1. Sums of Trues = Total number of Trues
+prediabetic_after_data = (data.loc[:,data.columns.str.contains("after",case=False)] > 140 ).sum(axis=1)
+
+prediabetic_row_datas = prediabetic_before_data + prediabetic_after_data
+
+prediabetic = prediabetic_row_datas >=3
+
+conditions=[
+    diabetic,
+    prediabetic
+]
+
+choices=[
+    "Diabetic",
+    "Prediabetic"
+]
+
+data["Status"] = np.select(conditions,choices,default="Normal")
 
 print(data)
